f2e959c6f5cb06e7293e5c032879a48314222ac0
Junhax <1018080220@qq.com>
2024-10-02 17:29:59 +0800

    Initial commit

---
+++ src/client/java/com/junhax/glassworld/GlassWorldClient.java
@@ -0,0 +1,10 @@
+package com.junhax.glassworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlassWorldClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
